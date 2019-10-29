bd0dcdb2db01fc9f298dffae01056bd87e21a058
Daniel Dosti <43644047+Danieldosti51@users.noreply.github.com>
2019-10-29 16:51:21 +0000

    removed unnecessary skip/clear when not in channel

--- src/main/java/events/MusicStopEvent.java
+++ src/main/java/events/MusicStopEvent.java
@@ -3,6 +3,7 @@ package events;
 import music.PlayerManager;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.managers.AudioManager;
 
 public class MusicStopEvent extends ListenerAdapter{
 
@@ -11,6 +12,13 @@ public class MusicStopEvent extends ListenerAdapter{
         if (!message.trim().equals("!skip")) {
             return;
         }
+
+        AudioManager audioManager = event.getGuild().getAudioManager();
+        if(!audioManager.isConnected()) {
+            event.getChannel().sendMessage("not connected to a voice channel").queue();
+            return;
+        }
+
         PlayerManager manager = PlayerManager.getInstance();
         manager.skipTrack(event.getChannel());
     }
